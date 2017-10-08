0122f19487b7145082893e311f867dc21e7d5697
sushanthavvaru <sushantha.a@gmail.com>
2017-10-08 04:07:45 -0700

    Change in spacing

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Started on port' + port);
+  console.log('Started on port ' + port);
 });
 
 module.exports = {app};
