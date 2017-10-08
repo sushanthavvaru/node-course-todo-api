37177de184339eeb90d8931d90d53db60634f6ad
sushanthavvaru <sushantha.a@gmail.com>
2017-10-08 05:06:21 -0700

    Landing page

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.get('/', (req, res) => {
+      res.send(<p>Welcome to Sushanth's Todo API Page. Contact <b>+1(619)-724-1262</b> to know how this works in detail or Email me at <b>sushantha.a@gmail.com</b></p>)
+    })
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
